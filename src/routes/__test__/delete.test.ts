5faa19ee0bdcb4d24b895ae348cef0100a971d4f
edumueller <eduardo.nicastro@icloud.com>
2021-08-27 11:27:48 -0300

    add delete event emit test

--- src/routes/__test__/delete.test.ts
+++ src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import mongoose from "mongoose";
 import { Order, OrderStatus } from "../../models/order";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns 404 if order does not exist", async () => {
   await request(app)
@@ -59,4 +60,26 @@ it("deletes (cancel) the order", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits an order:cancelled event");
+it("emits an order:cancelled event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
