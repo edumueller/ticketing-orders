6cc7e9c5afaf0975cd1fdad289e2abce370a3ebd
edumueller <eduardo.nicastro@icloud.com>
2021-08-26 23:36:53 -0300

    add delete route handler

--- src/routes/delete.ts
+++ src/routes/delete.ts
@@ -1,9 +1,32 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@devneering/common";
 import express, { Request, Response } from "express";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", (req: Request, res: Response) => {
-  res.send({});
-});
+router.delete(
+  "/api/orders/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
