ee351edd806da437903296e863a701bfbe83606e
edumueller <eduardo.nicastro@icloud.com>
2021-08-26 22:25:50 -0300

    add initial create route tests

---
+++ src/routes/__test__/new.test.ts
@@ -0,0 +1,5 @@
+it("returns an error if the ticket does not exist", async () => {});
+
+it("returns an error if the ticket is already reserved", async () => {});
+
+it("reserves a ticket", async () => {});
