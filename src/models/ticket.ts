c1ea0ef3790014d5bb3d659ebaa3837013610585
edumueller <eduardo.nicastro@icloud.com>
2021-08-27 17:43:51 -0300

    implement mongoose update version plugin

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { Order, OrderStatus } from "./order";
 
 interface TicketAttrs {
@@ -10,6 +11,7 @@ interface TicketAttrs {
 interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  version: number;
   isReserved(): Promise<boolean>;
 }
 
@@ -63,6 +65,9 @@ ticketSchema.methods.isReserved = async function () {
 
   return !!existingOrder;
 };
+
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
 
