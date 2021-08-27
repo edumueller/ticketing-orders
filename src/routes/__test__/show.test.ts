9a3690e5825baeefa55888d94d8d3ea79d138cb8
edumueller <eduardo.nicastro@icloud.com>
2021-08-26 23:28:52 -0300

    add fetch order tests

---
+++ src/routes/__test__/show.test.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import mongoose from "mongoose";
+
+it("returns 404 if ticket does not exist", async () => {
+  await request(app)
+    .get(`/api/orders/${mongoose.Types.ObjectId()}`)
+    .set("Cookie", global.signin())
+    .expect(404);
+});
+
+it("returns unauthorized if one user tries to fetch another users order", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const userOne = global.signin();
+  const userTwo = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", userTwo)
+    .send()
+    .expect(401);
+});
+
+it("fetches the order", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(fetchedOrder.id).toEqual(order.id);
+});
